ca2e2ce956cfce8824f92da3953a720102892f80
Beverly Ann Gupit <Bevzay@users.noreply.github.com>
2020-07-02 11:23:39 +1200

    Add exercise

--- src/AreaCalculator.java
+++ src/AreaCalculator.java
@@ -1,4 +1,20 @@
 public class AreaCalculator {
 
-    
+    /*
+     * Write a method named arena with one double parameter named radius
+     *
+     * The method needs to return a double value that represents the area of a circle
+     *
+     * Write another overloaded method with 2 parameters x and y (both doubles),
+     * where x and y represent the sides of a rectangle
+     *
+     * The method needs to return an area of a rectangle
+     *
+     * If either both parameters is/are a negative return -1.0 to indicate an invalid value
+     *
+     * TIP:
+     * - area of triangle: x * y
+     * - area of circle: radius * radius * PI
+     */
+
 }
