56bf5a6892efeeac11e6fead486534dedd31da44
Beverly Ann Gupit <Bevzay@users.noreply.github.com>
2020-07-02 11:35:03 +1200

    2nd method worked

--- src/AreaCalculator.java
+++ src/AreaCalculator.java
@@ -25,4 +25,12 @@ public class AreaCalculator {
         }
     }
 
+    public static double area(double x, double y) {
+        if(x < 0 || y < 0) {
+            return -1.0;
+        } else {
+            return x * y;
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("The area of a circle with r=5.0 is " + AreaCalculator.area(5.0));
+        System.out.println(AreaCalculator.area(-1));
+        System.out.println("The area of a rectangle with x=5.0 and y=4.0 is " + AreaCalculator.area(5.0, 4.0));
+        System.out.println(AreaCalculator.area(-1.0, 4.0));
 
     }
 }
